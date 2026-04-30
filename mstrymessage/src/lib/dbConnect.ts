545e5a32d543117d3d481fab35e56b9ef7a26a11
Sparsh Singh <sparshsinghjaduvanshi@gmail.com>
2026-04-30 23:46:59 +0530

    add database connection utility and environment configuration

---
+++ mstrymessage/src/lib/dbConnect.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+type ConnectionObject = {
+    isConnected?: number;
+}
+
+const connection: ConnectionObject = {}
+
+async function dbConnect(): Promise<void> {
+    if(connection.isConnected){
+        console.log("Already connected to database");
+        return;
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || "", {})
+        console.log(db.connections)
+        connection.isConnected = db.connections[0].readyState;
+
+        console.log("Connected to database");
+    } catch (error) {
+        console.error("Error connecting to database:", error);
+        
+        process.exit(1);
+    }
+}
+
+export default dbConnect;
